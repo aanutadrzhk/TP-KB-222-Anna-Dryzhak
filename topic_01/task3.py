2af6ab9a91a5b8689851e807a2c18258d26b13b2
aanutadrzhk <aanutadrzhk55@gmail.com>
2023-09-16 14:16:51 +0300

    add report file

--- topic_01/task3.py
+++ topic_01/task3.py
@@ -2,7 +2,7 @@ def findD():
     a = int(input("Please enter value a: "))
     b = int(input("Please enter value b: "))
     c = int(input("Please enter value c: "))
-    resalt = b**2 - 4*a*c
-    return resalt
+    result = b**2 - 4*a*c
+    return result
 
 print(findD())
