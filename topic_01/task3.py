50e37aa061e0f2d5218eb5c603dbb3f3acb4f199
aanutadrzhk <aanutadrzhk55@gmail.com>
2023-09-17 12:05:35 +0300

    changed task3

--- topic_01/task3.py
+++ topic_01/task3.py
@@ -1,8 +1,10 @@
-def findD():
-    a = int(input("Please enter value a: "))
-    b = int(input("Please enter value b: "))
-    c = int(input("Please enter value c: "))
+a = int(input("Please enter value a: "))
+b = int(input("Please enter value b: "))
+c = int(input("Please enter value c: "))
+
+def findD(a, b ,c):
+    
     result = b**2 - 4*a*c
     return result
 
-print(findD())
+print(findD(a, b ,c))
