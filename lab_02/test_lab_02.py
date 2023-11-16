ae8d0e269747a06ee458511ff7285075024a8685
aanutadrzhk <aanutadrzhk55@gmail.com>
2023-11-16 12:27:54 +0200

    changed test_lab_02

--- lab_02/test_lab_02.py
+++ lab_02/test_lab_02.py
@@ -7,8 +7,8 @@ class TestLab2Script(unittest.TestCase):
     def setUp(self):
         self.test_file = "test_lab2.csv"
         self.test_data = [
-            {"name": "John", "phone": "9675643454", "age": "21", "gender": "man"},
-            {"name": "Alice", "phone": "734586290", "age": "19", "gender": "woman"},
+            {"name": "Alice", "phone": "9675643454", "age": "21", "gender": "man"},
+            {"name": "John","phone": "734586290", "age": "19", "gender": "woman"},
         ]
 
 
@@ -28,7 +28,7 @@ class TestLab2Script(unittest.TestCase):
         self.assertEqual(loaded_data, self.test_data)
 
     def test_addNewElement(self):
-        with patch('builtins.input', side_effect=["John", "1234567890", "25", "male"]):
+        with patch('builtins.input', side_effect=["Bob", "1234567890", "25", "male"]):
             addNewElement(self.test_data)
             self.assertEqual(len(self.test_data), 3)
 
